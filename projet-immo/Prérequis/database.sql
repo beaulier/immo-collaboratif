8fda728509284c56ec1943c448276f69ef19cf92
thomas <beaulier.thomas@gmail.com>
2015-11-30 10:35:25 +0100

    modif database

--- projet-immo/Prérequis/database.sql
+++ projet-immo/Prérequis/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 27 Novembre 2015 à 11:46
+-- Généré le :  Lun 30 Novembre 2015 à 09:58
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS `profils` (
   `metier` varchar(30) NOT NULL,
   `sport` varchar(30) DEFAULT NULL,
   `film` varchar(30) DEFAULT NULL,
+  `image_profil` text NOT NULL,
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `state` varchar(1) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
@@ -42,10 +43,10 @@ CREATE TABLE IF NOT EXISTS `profils` (
 -- Contenu de la table `profils`
 --
 
-INSERT INTO `profils` (`nom`, `age`, `adresse`, `metier`, `sport`, `film`, `id`, `state`) VALUES
-('Martin', 24, '15 rue Hoche 94200 Ivry sur Seine', 'informaticien', 'foot', 'star wars', 0, '1'),
-('Jean', 26, '16 rue des Freres Blais 94200 Ivry sur Seine', 'facteur', 'foot', 'interstellar', 1, '1'),
-('Hollande', 32, '10 rue Lenine 94200 Ivry sur Seine', 'restaurateur', 'tennis', 'star wars', 2, '0');
+INSERT INTO `profils` (`nom`, `age`, `adresse`, `metier`, `sport`, `film`, `image_profil`, `id`, `state`) VALUES
+('Thomas', 24, '15 rue Hoche 94200 Ivry sur Seine', 'informaticien', 'foot', 'star wars', 'https://auth.etna-alternance.net/api/users/beauli_t/photo', 0, '1'),
+('Paul', 26, '16 rue des Freres Blais 94200 Ivry sur Seine', 'facteur', 'foot', 'interstellar', 'https://auth.etna-alternance.net/api/users/beltra_p/photo', 1, '1'),
+('Tristan', 32, '10 rue Lenine 94200 Ivry sur Seine', 'restaurateur', 'tennis', 'star wars', 'https://auth.etna-alternance.net/api/users/boitea_t/photo', 2, '0');
 
 -- --------------------------------------------------------
 
@@ -61,15 +62,15 @@ CREATE TABLE IF NOT EXISTS `zones` (
   `PRIX` text COLLATE utf8_bin NOT NULL,
   `URL_IMAGE` text COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `zones`
 --
 
 INSERT INTO `zones` (`ID`, `INTITULE`, `TAILLE`, `ADRESSE`, `PRIX`, `URL_IMAGE`) VALUES
-(1, 'Appartement 5 pieces', '70', '9 rue Louis Rousseau 94200 Ivry sur Seine', '255 000', 'http://c.visuels.poliris.com/bigs/c/1/2/d/c12d5561-3e04.jpg'),
-(2, 'Appartement 3 pieces', '50', '4 rue Rene Robin 94200 Ivry sur Seine', '190 000', 'http://b.visuels.poliris.com/bigs/b/6/2/8/b62898a4-4af4.jpg');
+(0, 'Appartement 5 pieces', '80', '9 rue Louis Rousseau 94200 Ivry sur Seine', '763 000', 'http://static.pap.fr/photos/A82/A82A1556.jpg'),
+(1, 'Appartement 3 pieces', '60', '4 rue Rene Robin 94200 Ivry sur Seine', '255 000', 'http://b.visuels.poliris.com/bigs/b/6/2/8/b62898a4-4af4.jpg');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
